c30fc6036f241e9e171d75a3eeefd4a193c88132
avcopan <avcopan@gmail.com>
2025-07-24 15:44:02 -0500

    Manually create workers on Bebop/CSED

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 """Local testing CLI."""
 
+import contextlib
 import os
 import socket
+import subprocess
 import warnings
+from pathlib import Path
 
 import click
 
@@ -59,6 +62,63 @@ def local(auto_config_flags: str | None = None):
 def sign():
     """Sign off on local tests."""
     tu.wrap_up_tests(from_archive=True, allow_override=True)
+
+
+# Helper function for manually creating workers on Bebop/CSED
+# (Not intended for use on other clusters, but could be generalized.)
+WORKER_DIR = Path(".workers")
+WORKER_NAME = "worker-{mem:d}g-{cpus:d}p-{index:02d}"
+WORKER_SCRIPT = """
+#!/bin/bash
+#PBS -l select=1:host={host}
+#PBS -N {name}
+#PBS -l walltime=01:00:00
+#PBS -q {queue} -A {account}
+
+hq worker start \\
+    --idle-timeout "10m" \\
+    --manager "pbs" \\
+    --cpus "{cpus:d}" \\
+    --resource "mem=sum({mem_mib})" \\
+    --on-server-lost "finish-running" \\
+    --time-limit "1h"
+"""
+
+
+@main.command("create-workers")
+@click.argument("host")
+@click.option("-n", "--number", default=1, help="Number of workers to create")
+@click.option("-m", "--mem", default=20, help="Memory (GB)")
+@click.option("-c", "--cpus", default=1, help="CPU count (#)")
+@click.option("-q", "--queue", default="csed", help="Queue name")
+@click.option("-A", "--account", default="g-CSE", help="Account name")
+def create_workers(
+    host: str,
+    number: int = 1,
+    mem: int = 20,
+    cpus: int = 1,
+    queue: str = "csed",
+    account: str = "g-CSE",
+):
+    """Create workers running on specific nodes.
+
+    This can be used for manual configuration of the HyperQueue server.
+    """
+    WORKER_DIR.mkdir(exist_ok=True)
+    with contextlib.chdir(WORKER_DIR):
+        for index in range(number):
+            name = WORKER_NAME.format(mem=mem, cpus=cpus, index=index)
+            script = Path(f"{name}.sh")
+            script_text = WORKER_SCRIPT.format(
+                host=host,
+                name=name,
+                cpus=cpus,
+                mem_mib=mechdriver.subtasks.memory_mib(mem),
+                queue=queue,
+                account=account,
+            )
+            script.write_text(script_text)
+            subprocess.run(["qsub", str(script)])
 
 
 if __name__ == "__main__":
--- src/mechdriver/subtasks/__init__.py
+++ src/mechdriver/subtasks/__init__.py
@@ -3,7 +3,7 @@
 from . import util
 from ._0setup import SUBTASK_DIR, setup, setup_multiple
 from ._1status import status, status_multiple
-from ._2run import setup_job, run_multiple
+from ._2run import memory_mib, run_multiple, setup_job
 from ._3view import display
 
 __all__ = [
@@ -16,4 +16,5 @@ __all__ = [
     "run_multiple",
     "util",
     "display",
+    "memory_mib",
 ]
